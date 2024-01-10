16bff3d305ded83dcc4be716394d488a6755ff16
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-01-10 23:49:50 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import PrelineScript from "./components/PrelineUI/PrelineScript";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'IEEE SRMIST Student Branch',
+  description: 'Advancing Technology for the Betterment of Humanity',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+      <PrelineScript />
+    </html>
+  )
+}
