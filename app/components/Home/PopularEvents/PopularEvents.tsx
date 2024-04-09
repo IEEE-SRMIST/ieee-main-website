d14d4d5b752b5485ffb6e2c99e5b570996019095
kristentalukdar <81560832+kristentalukdar@users.noreply.github.com>
2024-04-09 18:51:01 +0530

    Updated Style

--- app/components/Home/PopularEvents/PopularEvents.tsx
+++ app/components/Home/PopularEvents/PopularEvents.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const PopularEvents = () => {
@@ -5,9 +6,19 @@ const PopularEvents = () => {
         <div>
 
 
-            <div className="bg-blue-100 max-w-screen-2xl mx-auto px-4 py-6 md:px-6 md:py-8 xl:px-8 xl:py-10">
-
-                <div className="grid lg:grid-cols-2 gap-6">
+            <div className="bg-blue-400 max-w-screen-2xl mx-auto px-4 py-6 md:px-6 md:py-8 xl:px-8 xl:py-10">
+                <div className="bg-gray-200 mx-auto mb-10 items-center justify-center text-center">
+                    <h3 className="mb-2 sm:mb-4 font-sans font-semibold text-blue-800 text-xs lg:text-base">
+                        Popular Events
+                    </h3>
+                    <h2 className="mb-4 md:mb-6 font-sans font-bold text-black text-2xl sm:text-4xl">
+                        Know more about us
+                    </h2>
+                    <h4 className="mx-auto max-w-[64rem] font-sans font-normal text-black text-sm sm:text-lg">
+                        Join a community of more than 450,000 technology and engineering professionals united by a common desire to continuously learn, interact, collaborate, and innovate
+                    </h4>
+                </div>
+                <div className="grid lg:grid-cols-2 gap-8">
 
                     <a
                         className="group sm:flex rounded-xl dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -24,33 +35,18 @@ const PopularEvents = () => {
                             <div className="p-4 flex flex-col h-full sm:p-6">
                                 <div className="mb-3">
                                     <p className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
-                                        Business
+
+                                        <Link href="hacktrix.ieeesrmist.com">HackTrix</Link>
                                     </p>
                                 </div>
-                                <h3 className="text-lg sm:text-2xl font-semibold text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-                                    Preline becomes an official Instagram Marketing Partner
-                                </h3>
-                                <p className="mt-2 text-gray-600 dark:text-gray-400">
-                                    Great news we're eager to share.
+                                <h4 className="font-sans font-semibold text-black text-2xl sm:text-4xl  group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
+                                    HACKTRIX
+                                </h4>
+                                <p className="mt-2 mb-2 font-sans font-normal text-black text-justify lg:text-base">
+                                    HackTrix, IEEE SRM Student Branch's premier hackathon, invites participants to unleash their
+                                    creativity through coding. Over three days, competitors will engage in a coding frenzy, striving to ascend the leaderboard .
                                 </p>
                                 <div className="mt-5 sm:mt-auto">
-
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <img
-                                                className="h-[2.875rem] w-[2.875rem] rounded-full"
-                                                src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                                                alt="Image Description"
-                                            />
-                                        </div>
-                                        <div className="ms-2.5 sm:ms-4">
-                                            <h4 className="font-semibold text-gray-800 dark:text-gray-200">
-                                                Aaron Larsson
-                                            </h4>
-                                            <p className="text-xs text-gray-500">Feb 15, 2021</p>
-                                        </div>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
@@ -63,7 +59,7 @@ const PopularEvents = () => {
                         <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full h-[200px] sm:w-[250px] sm:h-[350px]">
                             <img
                                 className="w-full h-full absolute top-0 start-0 object-cover"
-                                src="https://images.unsplash.com/photo-1669824774762-65ddf29bee56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                                src="https://images.unsplash.com/photo-1664574654529-b60630f33fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
                                 alt="Image Description"
                             />
                         </div>
@@ -71,37 +67,93 @@ const PopularEvents = () => {
                             <div className="p-4 flex flex-col h-full sm:p-6">
                                 <div className="mb-3">
                                     <p className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
-                                        Announcements
+
+                                        <Link href="concepto.ieeesrmist.in">Concepto</Link>
                                     </p>
                                 </div>
-                                <h3 className="text-lg sm:text-2xl font-semibold text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-                                    Announcing a free plan for small teams
-                                </h3>
-                                <p className="mt-2 text-gray-600 dark:text-gray-400">
-                                    At Wake, our mission has always been focused on bringing openness.
+                                <h4 className="font-sans font-semibold text-black text-2xl sm:text-4xl group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
+                                    CONCEPTO
+                                </h4>
+                                <p className="mt-2 mb-2 font-sans font-normal text-black text-justify lg:text-base">
+                                    Concepto, a technical event organized by the TECHNOCRATS TEAM of IEEE SRM Student
+                                    Branch, offers participants an opportunity to delve into PLC programming basics and its
+                                    practical applications.
                                 </p>
                                 <div className="mt-5 sm:mt-auto">
-
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <img
-                                                className="h-[2.875rem] w-[2.875rem] rounded-full"
-                                                src="https://images.unsplash.com/photo-1669720229052-89cda125fc3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                                                alt="Image Description"
-                                            />
-                                        </div>
-                                        <div className="ms-2.5 sm:ms-4">
-                                            <h4 className="font-semibold text-gray-800 dark:text-gray-200">
-                                                Hanna Wolfe
-                                            </h4>
-                                            <p className="text-xs text-gray-500">Feb 4, 2021</p>
-                                        </div>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
                     </a>
+
+                    <a
+                        className="group sm:flex rounded-xl dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        href="#"
+                    >
+                        <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full h-[200px] sm:w-[250px] sm:h-[350px]">
+                            <img
+                                className="w-full h-full absolute top-0 start-0 object-cover"
+                                src="https://images.unsplash.com/photo-1664574654529-b60630f33fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                                alt="Image Description"
+                            />
+                        </div>
+                        <div className="grow">
+                            <div className="p-4 flex flex-col h-full sm:p-6">
+                                <div className="mb-3">
+                                    <p className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+
+                                        <Link href="hacktrix.ieeesrmist.com">Technozest</Link>
+                                    </p>
+                                </div>
+                                <h4 className="font-sans font-semibold text-black text-2xl sm:text-4xl group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
+                                    TECHNOZEST
+                                </h4>
+                                <p className="mt-2 mb-2 font-sans font-normal text-black text-justify lg:text-base">
+                                    Techno Zest, hosted by IEEE SRMIST Student Branch, is an event brimming with tech events
+                                    and workshops. Participants delve into various aspects of technology, from coding to robotics,
+                                    through engaging sessions and hands-on activities. It's a platform for students to enhance their
+                                    skills, exchange knowledge, and foster innovation in a dynamic setting.
+                                </p>
+                                <div className="mt-5 sm:mt-auto">
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a
+                        className="group sm:flex rounded-xl dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        href="#"
+                    >
+                        <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full h-[200px] sm:w-[250px] sm:h-[350px]">
+                            <img
+                                className="w-full h-full absolute top-0 start-0 object-cover"
+                                src="https://images.unsplash.com/photo-1664574654529-b60630f33fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                                alt="Image Description"
+                            />
+                        </div>
+                        <div className="grow">
+                            <div className="p-4 flex flex-col h-full sm:p-6">
+                                <div className="mb-3">
+                                    <p className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+
+                                        <Link href="hacktrix.ieeesrmist.com">Hyperforge</Link>
+                                    </p>
+                                </div>
+                                <h4 className="font-sans font-semibold text-black text-2xl sm:text-4xl group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
+                                    HYPERFORGE
+                                </h4>
+                                <p className="mt-2 mb-2 font-sans font-normal text-black text-justify lg:text-base">
+                                    Hyper Forge, organized by IEEE SRMIST Student Branch, is a blockchain workshop aimed at
+                                    equipping participants with in-depth knowledge and practical skills in blockchain technology.
+                                    Hyper Forge offers a unique opportunity for individuals to delve into the intricacies of blockchain
+                                    technology and its potential for revolutionizing industries.
+                                </p>
+                                <div className="mt-5 sm:mt-auto">
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
 
                 </div>
 
--- app/components/Home/RecentBlogs/RecentBlogs.tsx
+++ app/components/Home/RecentBlogs/RecentBlogs.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const RecentBlogs = () => {
@@ -7,17 +8,20 @@ const RecentBlogs = () => {
 
             <div className="bg-green-100 max-w-screen-2xl mx-auto px-4 py-6 md:px-6 md:py-8 xl:px-8 xl:py-10">
 
-                <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-                    <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-                        Insights
+                <div className="bg-gray-200 mx-auto mb-10 items-center justify-center text-center">
+                    <h3 className="mb-2 sm:mb-4 font-sans font-semibold text-blue-800 text-xs lg:text-base">
+                        BLOGS
+                    </h3>
+                    <h2 className="mb-4 md:mb-6 font-sans font-bold text-black text-2xl sm:text-4xl">
+                        Know more about us
                     </h2>
-                    <p className="mt-1 text-gray-600 dark:text-gray-400">
-                        Stay in the know with insights from industry experts.
-                    </p>
+                    <h4 className="mx-auto max-w-[64rem] font-sans font-normal text-black text-sm sm:text-lg">
+                        Join a community of more than 450,000 technology and engineering professionals united by a common desire to continuously learn, interact, collaborate, and innovate
+                    </h4>
                 </div>
 
-                <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
- 
+                <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-6">
+
                     <a
                         className="group dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                         href="#"
@@ -28,20 +32,21 @@ const RecentBlogs = () => {
                                 src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"
                                 alt="Image Description"
                             />
-                            <span className="absolute top-0 end-0 rounded-se-xl rounded-es-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-gray-900">
-                                Sponsored
-                            </span>
+
                         </div>
                         <div className="mt-7">
-                            <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
-                                Studio by Preline
+                            <h3 className="font-sans font-semibold text-black text-xl sm:text-2xl group-hover:text-gray-600 dark:text-gray-200">
+                                Award-Winning Work of the 2023 Chemistry:Nobel Prize
                             </h3>
-                            <p className="mt-3 text-gray-800 dark:text-gray-200">
-                                Produce professional, reliable streams easily leveraging Preline's
-                                innovative broadcast studio
+                            <p className="mt-3 font-sans font-normal text-black text-justify lg:text-base">
+                                In a groundbreaking announcement, the 2023 Chemistry Nobel Prize was awarded to a
+                                distinguished team of researchers for their pioneering work on Quantum Dots. These nano-sized
+                                semiconductor particles have redefined the boundaries of science and technology, ushering in a new era
+                                of possibilities across various industries.
                             </p>
                             <p className="mt-5 inline-flex items-center gap-x-1 text-blue-600 decoration-2 group-hover:underline font-medium">
-                                Read more
+                                <Link href="https://ieeesrm.medium.com/understanding-quantum-dots-the-award-winning-work-of-the-2023-chemistry-nobel-prize-503fd6ece551">Read more</Link>
+
                                 <svg
                                     className="flex-shrink-0 w-4 h-4"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -72,15 +77,18 @@ const RecentBlogs = () => {
                             />
                         </div>
                         <div className="mt-7">
-                            <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
-                                Onsite
+                            <h3 className="font-sans font-semibold text-black text-xl sm:text-2xl group-hover:text-gray-600 dark:text-gray-200">
+                                Embracing the Future: The Power of Adaptive AI
                             </h3>
-                            <p className="mt-3 text-gray-800 dark:text-gray-200">
-                                Optimize your in-person experience with best-in-class capabilities
-                                like badge printing and lead retrieval
+                            <p className="mt-3 font-sans font-normal text-black text-justify lg:text-base">
+                                Artificial Intelligence has reached a remarkable inflection point with the emergence of
+                                Adaptive AI. Unlike its conventional counterparts, Adaptive AI possesses the remarkable ability to learn,
+                                evolve, and adjust in real-time based on changing data and circumstances. This groundbreaking
+                                technology is poised to revolutionize industries and elevate user experiences.
                             </p>
+
                             <p className="mt-5 inline-flex items-center gap-x-1 text-blue-600 decoration-2 group-hover:underline font-medium">
-                                Read more
+                                <Link href="https://ieeesrm.medium.com/embracing-the-future-the-power-of-adaptive-ai-892137b3bfc6">Read more</Link>
                                 <svg
                                     className="flex-shrink-0 w-4 h-4"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -99,36 +107,6 @@ const RecentBlogs = () => {
                         </div>
                     </a>
 
-                    <a
-                        className="group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg transition bg-[url('https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80')] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                        href="#"
-                    >
-                        <div className="flex-auto p-4 md:p-6">
-                            <h3 className="text-xl text-white/[.9] group-hover:text-white">
-                                <span className="font-bold">Preline</span> Press publishes books
-                                about economic and technological advancement.
-                            </h3>
-                        </div>
-                        <div className="pt-0 p-4 md:p-6">
-                            <div className="inline-flex items-center gap-2 text-sm font-medium text-white group-hover:text-white/[.7]">
-                                Visit the site
-                                <svg
-                                    className="flex-shrink-0 w-4 h-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={24}
-                                    height={24}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path d="m9 18 6-6-6-6" />
-                                </svg>
-                            </div>
-                        </div>
-                    </a>
 
                 </div>
 
