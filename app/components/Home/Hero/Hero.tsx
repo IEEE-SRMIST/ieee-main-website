2e4260fe99816d2406a00d8da71cb65a361d5c0a
Lakshaya Krishnaraj <lakshayakrishnaraj7@gmail.com>
2024-04-15 11:31:38 +0530

    updating hero

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -19,13 +19,10 @@ const Hero = () => {
   return (
     <div className="max-w-screen-2xl mx-auto px-4 py-6 md:px-6 md:py-8 xl:px-8 xl:py-10">
       <section className="relative flex flex-1 items-center justify-center overflow-hidden rounded-lg bg-gray-100 shadow-lg py-36 md:py-48">
-        <motion.img
-          src="/img/parallax.jpg.jpg"
-          loading="lazy"
-          alt="IEEE SRM SB"
-          className="absolute inset-0 h-full w-full object-cover object-center"
-          style={{ y: -scrollY / 2, scale: 1.5 }}
-        />
+        <video autoPlay muted loop className="absolute inset-0 h-full w-full object-cover object-center">
+          <source src="/vid/hero.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <motion.div
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
@@ -38,32 +35,8 @@ const Hero = () => {
             transition={{ delay: 0.5, duration: 1 }}
             className="mb-4 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl"
           >
-            Innovate. Edify. Experience. Excel
+            ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ 
           </motion.h1>
-          <motion.div
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ delay: 1, duration: 0.5 }}
-            className="animate-bounce text-white"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10 mx-auto"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <motion.path
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 1.5 }}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 14l-7 7m0 0l-7-7m7 7V3"
-              />
-            </svg>
-          </motion.div>
         </motion.div>
       </section>
     </div>
